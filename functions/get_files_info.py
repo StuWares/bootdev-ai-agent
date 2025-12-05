e9069bebf3fc004229fd15b589eaa896eb352f35
StuWares <stuwares@outlook.com>
2025-12-05 16:19:12 +0000

    Adding get_file_content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,16 +1,13 @@
 import os
 def get_files_info(working_directory, directory="."):
-
+    working_dir_abs = os.path.abspath(working_directory)
     full_path = os.path.join(working_directory, directory)
     dir_abs = os.path.abspath(full_path)
     output_string = ""
 
-    #print(f"full path: {full_path}")
-    #print(f"Working dir: {working_directory}")
-    #print(f"dir abs : {dir_abs}")
 
     # Ensure that the target directory is within the working directory - prevents the LLM from going rogue!
-    if working_directory not in dir_abs:
+    if not dir_abs.startswith(working_dir_abs):
         return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
     
     if not os.path.isdir(dir_abs):
@@ -21,7 +18,6 @@ def get_files_info(working_directory, directory="."):
         for file in dir_list:
             output_string += f"- {file}: file_size={os.path.getsize(os.path.join(dir_abs,file))}, is_dir={os.path.isdir(os.path.join(dir_abs,file))}\n"
         
-        #print(output_string)
         return output_string
     except Exception as e:
         return f"Error listing files: {e}"
