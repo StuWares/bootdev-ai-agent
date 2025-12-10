5e9def77866f6441755d1ff35e71081fd385ee77
StuWares <stuwares@outlook.com>
2025-12-10 16:59:28 +0000

    add first function declaration

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -27,18 +28,28 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
     )
 
     if response.usage_metadata == None:
         raise RuntimeError("usage_metadata is empty, likely due to a failed API response")
     
+    
+
+    
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        
-    print(response.text)
+    
+
+    if not response.function_calls:
+        print(response.text)
+        return
+    
+    
+    for call in response.function_calls:
+        print(f"Calling function: {call.name}({call.args})")
 
 
 if __name__ == "__main__":
